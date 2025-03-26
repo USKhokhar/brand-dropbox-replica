8dabdd4b50f1a7a80900032ab9dbda70ae70a7b0
USK <63156376+USKhokhar@users.noreply.github.com>
2025-03-26 07:40:29 +0000

    navigation grid layout setup

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 
 import LogoXS from '@/assets/logo-xs.svg'
@@ -5,40 +6,41 @@ import Card from './ui/nav-card';
 
 const NavScreen = () => {
     return (
-        <div className="mx-auto w-screen p-1 h-screen min-h-screen rounded grid gap-1 md:grid-rows-2 md:grid-cols-4 grid-rows-4 grid-cols-2">
+        <div className="mx-auto w-screen p-1 h-screen min-h-screen rounded grid gap-1 md:gap-2 
+            md:grid-rows-[2fr_0.5fr_2fr] md:grid-cols-[2fr_2fr_0.5fr_2fr_2fr] grid-rows-[2fr_2fr_0.5fr_2fr_2fr] grid-cols-[2fr_0.5fr_2fr]">
             <Card
                 heading='framework'
-                className='bg-fw-background rounded-tl-md'
+                className='bg-fw-background rounded-tl-md md:row-span-2 row-start-1 col-start-1'
                 headingClassName='text-fw-text'
             >
                 <LogoXS />
             </Card>
 
-            <Card heading='Voice & Tone' className='rounded-tr-md md:rounded-none bg-vt-background' headingClassName='text-vt-text'>
+            <Card heading='Voice & Tone' className='rounded-tr-md md:rounded-none bg-vt-background col-span-2 row-start-1 col-start-2' headingClassName='text-vt-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Logo' className='bg-logo-background' headingClassName='text-logo-text'>
+            <Card heading='Logo' className='bg-logo-background col-span-2 md:col-span-1 md:row-span-2 md:col-start-4 row-start-2 md:row-start-1 col-start-1' headingClassName='text-logo-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Typography' className='bg-typo-background' headingClassName='text-typo-text'>
+            <Card heading='Typography' className='bg-typo-background md:rounded-tr-md row-span-2 md:row-span-1 row-start-2 md:row-start-1 md:col-start-5 col-start-3' headingClassName='text-typo-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Iconography' className='bg-icon-background' headingClassName='text-typo-text'>
+            <Card heading='Iconography' className='bg-icon-background md:rounded-bl-md row-span-2 row-start-3 col-start-1 md:row-start-3 md:col-start-1 md:row-span-1' headingClassName='text-typo-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Color' className='bg-clr-background' headingClassName='text-clr-text'>
+            <Card heading='Color' className='bg-clr-background col-span-2 col-start-2 row-start-4 md:col-span-1 md:row-span-full md:row-start-2 md:col-start-2' headingClassName='text-clr-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Imagery' className='bg-img-background rounded-bl-md md:rounded-none' headingClassName='text-img-text'>
+            <Card heading='Imagery' className='bg-img-background rounded-bl-md md:rounded-none col-start-1 row-start-5 col-span-2 md:row-start-3 md:col-start-3 md:col-span-2' headingClassName='text-img-text'>
                 <LogoXS />
             </Card>
 
-            <Card heading='Motion' className='bg-motion-background rounded-br-md' headingClassName='text-motion-text'>
+            <Card heading='Motion' className='bg-motion-background rounded-br-md col-start-3 row-start-5 md:row-span-2 md:row-start-2 md:col-start-5 ' headingClassName='text-motion-text'>
                 <LogoXS />
             </Card>
         </div>
--- components/ui/nav-card.tsx
+++ components/ui/nav-card.tsx
@@ -12,12 +12,12 @@ const Card = ({ heading, children, className, headingClassName }: CardProps) => 
   return (
     <div
       className={cn(
-        "group grow cursor-pointer hover:bg-card-background-hover transition-all duration-300 w-full h-full px-3 py-1.5 flex flex-col justify-between gap-2",
+        "group grow cursor-pointer hover:bg-card-background-hover transition-all duration-300 w-full h-full px-4 py-2 flex flex-col justify-between gap-2",
         className
       )}
     >
         <h4 className={cn(
-            "font-medium text-xl capitalize group-hover:text-white group-hover:font-bold transition-all duration-300", 
+            "font-medium text-xl xs:text-2xl sm:text-3xl md:text-4xl capitalize group-hover:text-white group-hover:font-bold transition-all duration-300", 
             headingClassName
         )}>
             {heading}
